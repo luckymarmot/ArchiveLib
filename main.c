e20fb163560ecd04f0c078e3877145f75e968344
Matthaus Woolard <matthaus.woolard@gmail.com>
2017-02-01 13:42:25 +0000

    test same and reopen

--- main.c
+++ main.c
@@ -21,18 +21,30 @@
 int main() {
     Archive* archive = (Archive*) (malloc(sizeof(Archive)));
     Archive__init__(archive, "./");
-    Errors error = Archive_add_page_by_name(archive, "fe644dd2-185c-40d0-acdf-4e255d44331a");
-    //Archive_new_page(archive);
-    printf("error? %d\n", error);
+    Archive_new_page(archive);
     char key[20] = {21, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    printf("new page made");
     Archive_set(archive, key, "the data", 8);
     char key2[20] = {124, 124, 124, 124, 124, 124, 124, 124, 124, 124, 124, 124, 124, 124, 124, 124, 124, 124, 124, 124};
     Archive_set(archive, key2, "the other data", 14);
     ArchiveFiles* files = (ArchiveFiles*) (malloc(sizeof(ArchiveFiles)));
     Archive_save(archive, files);
+    char* filename;
     for (int i = 0; i < files->n_files; ++i) {
+        filename = files->page_filenames[i] + 2;
         printf("file %s\n", files->page_filenames[i]);
     }
     Archive_free(archive);
+
+
+    archive = (Archive*) (malloc(sizeof(Archive)));
+    Archive__init__(archive, "./");
+    Errors error = Archive_add_page_by_name(archive, filename);
+    printf("error? %d\n", error);
+    Archive_set(archive, key, "the data", 8);
+    Archive_set(archive, key2, "the other data", 14);
+    Archive_save(archive, files);
+    Archive_free(archive);
+
     return 0;
 }
