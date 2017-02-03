fe1132bc7a207ab79c84afbc2ca3e84234694de1
Matthaus Woolard <matthaus.woolard@gmail.com>
2017-02-03 14:59:31 +0000

    update tests

--- test_archive/basic_test.c
+++ test_archive/basic_test.c
@@ -54,7 +54,6 @@ static void test_Archive_init(void **state) {
     Archive archive;
     Archive_init(&archive, "./");
     assert_int_equal(archive.n_pages, 0);
-    assert_null(archive.page_stack);
     assert_string_equal(archive.base_file_path, "./");
 
     // Test that the file name is copied.
@@ -76,16 +75,14 @@ static void test_Archive_free(void **state) {
     Archive_init(&archive, "./");
     Archive_add_empty_page(&archive);
     assert_int_equal(archive.n_pages, 1);
-    assert_non_null(archive.page_stack);
+    assert_non_null(archive.pages);
     assert_string_equal(archive.base_file_path, "./");
 
-    ArchiveListItem* page_stack = archive.page_stack;
-    ArchivePage* page = archive.page_stack->page;
+    ArchivePage* page = &(archive.pages[0]);
     char* filename = page->filename;
     HashIndex* index = page->index;
 
     assert_int_not_equal(malloc_size(page), 0);
-    assert_int_not_equal(malloc_size(page_stack), 0);
     assert_int_not_equal(malloc_size(filename), 0);
     assert_int_not_equal(malloc_size(index), 0);
 
@@ -96,8 +93,6 @@ static void test_Archive_free(void **state) {
     assert_int_equal(archive.n_pages, 0);
 
     assert_int_equal(malloc_size(page), 0);
-    assert_int_equal(malloc_size(page_stack), 0);
-    assert_int_equal(malloc_size(archive.page_stack), 0);
     assert_int_equal(malloc_size(archive.base_file_path), 0);
 
     assert_int_equal(malloc_size(filename), 0);
@@ -121,7 +116,7 @@ static void test_Archive_has(void **state) {
 
     assert_false(Archive_has(&archive, key));
 
-    HashIndex* index = archive.page_stack->page->index;
+    HashIndex* index = (&(archive.pages[0]))->index;
     HashIndex_set(index, key, 0, 0);
 
     assert_true(Archive_has(&archive, key));
@@ -151,7 +146,7 @@ static void test_Archive_has(void **state) {
 
     // Insect in the middle of the stack
     HashIndex_set(
-            archive.page_stack->next->page->index,
+            (&(archive.pages[1]))->index,
             key3, 1, 1
     );
 
@@ -165,7 +160,7 @@ static void test_Archive_has(void **state) {
     };
     assert_false(Archive_has(&archive, key4));
     HashIndex_set(
-            archive.page_stack->next->page->index,
+            (&(archive.pages[1]))->index,
             key4, 1, 1
     );
     assert_true(Archive_has(&archive, key4));
@@ -179,7 +174,7 @@ static void test_Archive_has(void **state) {
     };
     assert_false(Archive_has(&archive, key5));
     HashIndex_set(
-            archive.page_stack->next->page->index,
+            (&(archive.pages[1]))->index,
             key5, 1, 1
     );
     assert_true(Archive_has(&archive, key5));
@@ -253,7 +248,6 @@ static void test_archive_init(void **state) {
     Archive_init(&archive, "./");
     assert_int_equal(archive.n_pages, 0);
     assert_string_equal(archive.base_file_path, "./");
-    assert_null(archive.page_stack);
     Archive_free(&archive);
 }
 
@@ -264,27 +258,14 @@ static void test_archive_new_file(void **state) {
     Archive_add_empty_page(&archive);
     assert_int_equal(archive.n_pages, 1);
     assert_string_equal(archive.base_file_path, "./");
-    assert_non_null(archive.page_stack);
-    void* pstack1 = archive.page_stack;
-    assert_null(archive.page_stack->next);
+    assert_non_null(archive.pages);
+    void* pstack1 = &(archive.pages[0]);
 
     // Add a new page
     Archive_add_empty_page(&archive);
     assert_int_equal(archive.n_pages, 2);
     assert_string_equal(archive.base_file_path, "./");
-    assert_non_null(archive.page_stack);
-    assert_ptr_not_equal(pstack1, archive.page_stack);
-    // the new page linked list should point to the old linked list
-    assert_ptr_equal(archive.page_stack->next, pstack1);
-    void* pstack2 = archive.page_stack;
 
-    // Add a new page
-    Archive_add_empty_page(&archive);
-    assert_int_equal(archive.n_pages, 3);
-    assert_non_null(archive.page_stack);
-    assert_ptr_not_equal(pstack2, archive.page_stack);
-    // the new page linked list should point to the old linked list
-    assert_ptr_equal(archive.page_stack->next, pstack2);
 
     Archive_free(&archive);
 }
@@ -296,10 +277,9 @@ static void test_ArchivePage_init_saves_head(void **state) {
     Archive_add_empty_page(&archive);
     assert_int_equal(archive.n_pages, 1);
     assert_string_equal(archive.base_file_path, "./");
-    assert_non_null(archive.page_stack);
-    assert_null(archive.page_stack->next);
+    assert_non_null(archive.pages);
 
-    FILE* f = fopen(archive.page_stack->page->filename, "r+");
+    FILE* f = fopen(archive.pages[0].filename, "r+");
     char* data = (char*) malloc(sizeof(char)*10);
     size_t result = fread(data, 1, 10, f);
 
@@ -319,9 +299,9 @@ static void test_ArchivePage_open_file_locks_file(void **state) {
     Archive_add_empty_page(&archive);
 
     // There should be a lock on the file
-    int response = lockf(archive.page_stack->page->fd, F_TEST, 0);
+    int response = lockf(archive.pages[0].fd, F_TEST, 0);
     assert_int_equal(response, -1);
-    file_descriptor fd = open(archive.page_stack->page->filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    file_descriptor fd = open(archive.pages[0].filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
 
     Archive_free(&archive);
 
@@ -340,20 +320,15 @@ static void test_HashIndex_init(void **state) {
     Archive archive;
     Archive_init(&archive, "./");
     Archive_add_empty_page(&archive);
-    HashIndex* index = archive.page_stack->page->index;
+    HashIndex* index = archive.pages[0].index;
     assert_non_null(index);
     assert_int_equal(index->n_items, 0);
-    for (int i = 0; i < 256; ++i) {
-        assert_null(index->pages[i]);
-    }
+
     Archive_free(&archive);
 
     HashIndex indexL;
     HashIndex_init(&indexL);
     assert_int_equal(indexL.n_items, 0);
-    for (int i = 0; i < 256; ++i) {
-        assert_null(indexL.pages[i]);
-    }
 
 }
 
